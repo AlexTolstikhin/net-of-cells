daae92a7e3f7b9534155bb5c801115a37bd1e830
Eldanchik <Eldanchik@Eldanas-MacBook.local>
2020-02-23 17:31:41 -0800

    added basic template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+const data = {
+  0: {
+    0: {
+      id: '1'
+    },
+    1: {
+      id: '2'
+    },
+    2: {
+      id: '3'
+    }
+  },
+  1: {
+    0: {
+      id: '4'
+    },
+    1: {
+      id: '5'
+    },
+    2: {
+      id: '6'
+    }
+  },
+  2: {
+    0: {
+      id: '7'
+    },
+    1: {
+      id: '8'
+    },
+    2: {
+      id: '9'
+    }
+  }
+};
+
+
+const styles = {
+  app: {
+    width: '200px',
+    margin: '0 auto'
+  },
+  column: {
+    height: '50px',
+    width: '50px',
+    border: '1px solid'
+  },
+  row: {
+    display: 'flex',
+    justifyContent: 'center'
+  }
+}
+
+const renderColumn = (data, row, col) => <div style={styles.column}>{col.id}</div>
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={styles.app}>
+    {Object.keys(data).map(row => 
+      <div style={styles.row}>{Object.keys(data[row]).map(item => renderColumn(data[row][item]))}</div>
+    )}
     </div>
   );
 }
